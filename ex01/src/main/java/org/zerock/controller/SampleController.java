55397178409b0080097fc76d234ae159fd0b729d
MooMoongg <alchemist72727@gmail.com>
2025-02-19 14:49:02 +0900

    feat: complate the part2(fileUpload)

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -1,12 +1,13 @@
 package org.zerock.controller;
 
-import org.springframework.http.HttpHeaders;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,10 +15,12 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
 import org.zerock.domain.TodoDTO;
@@ -141,6 +144,19 @@ public class SampleController {
 		return new ResponseEntity<>(msg, header, HttpStatus.OK);
 	}
 	
+	@GetMapping("/exUpload")
+	public void exUpload() {
+		log.info("/exUpload.................");
+	}
 	
+	@PostMapping("/exUploadPost")
+	public void exUploadPost(ArrayList<MultipartFile> files) {
+		
+		files.forEach(file -> {
+			log.info("-----------------------------");
+			log.info("name:"+file.getOriginalFilename());
+			log.info("size:"+file.getSize());
+		});
+	}
 	
 }
