48105865aaabef07568f988126f85f5b8a228312
MooMoongg <alchemist72727@gmail.com>
2025-02-24 14:32:28 +0900

    test: 게시글 CRUD Junit4 테스트

---
+++ ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,62 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.Setter;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
+
+
+
+@Service
+@Log4j
+
+public class BoardServiceImpl implements BoardService {
+
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+
+
+	@Override
+	public void register(BoardVO board) {
+		
+		log.info("register....."+board);
+		
+		mapper.insertSelectKey(board);
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		
+		log.info("getList.......이거는 BoardServiceImpl의 로그");
+		return mapper.getList();
+	}
+	
+	@Override
+	public BoardVO get(Long bno) {
+		
+		log.info("get.......이거는 BoardServiceImpl의 로그"+bno);
+		return mapper.read(bno);
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		
+		log.info("modify.......이거는 BoardServiceImpl의 로그"+board);
+		return mapper.update(board)==1;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		
+		log.info("modify.......이거는 BoardServiceImpl의 로그"+bno);
+		return mapper.delete(bno)==1;
+	}
+
+
+}
